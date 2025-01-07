33c733e07087fd7e94762ae5541c6472cc2512e1
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-01-07 17:11:53 +0530

    Changes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Include Vite's root HTML
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JavaScript/TypeScript/React files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
