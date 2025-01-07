65664c27e85d3963b6d11c49c185dc3e717c26ca
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-01-08 03:26:44 +0530

    Login page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,40 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Layout from "./components/Layout.jsx"; // Separate Layout component
+
+import Task from "./pages/Task";
+import Trash from "./pages/Trash";
+import TaskDetails from "./pages/TaskDetails";
+import User from "./pages/User";
+import Login from "./pages/Login";
+import { Toaster } from "sonner";
+import Dshboard from "./pages/Dshboard.jsx";
+
+const App = () => {
+  return (
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        {/* Protected Routes */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dshboard />} />
+          <Route path="/tasks" element={<Task />} />
+          <Route path="/completed/:status" element={<Task />} />
+          <Route path="/in-progress/:status" element={<Task />} />
+          <Route path="/todo/:status" element={<Task />} />
+          <Route path="/team" element={<User />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Route>
+
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      {/* Toast notifications */}
+      <Toaster richColors />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/pages/Dshboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dshboard() {
+  return <div>Dshboard</div>;
+}
+
+export default Dshboard;
